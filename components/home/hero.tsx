c712d69c614db4b891a1675fb23490d12ef2825e
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-09-29 21:25:18 +0600

    cta btn added

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import { Space_Grotesk } from 'next/font/google';
+import { FaMapLocationDot } from 'react-icons/fa6';
+import Link from 'next/link';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
 const Hero = () => {
   return (
-    <section className='section-padding container grid min-h-screen grid-rows-2 gap-10'>
+    <section className='section-padding container relative grid min-h-screen grid-rows-2 gap-10'>
       {/* TOP */}
       <div className='grid h-full w-full grid-cols-[20rem_auto_30rem] gap-5'>
         {/* LEFT */}
@@ -108,6 +110,19 @@ const Hero = () => {
           </div>
         </div>
       </div>
+
+      {/* CTA BUTTON */}
+      <Link
+        href='/'
+        className='eq group absolute left-1/2 top-1/2 z-[1] h-28 w-28 -translate-x-1/2 -translate-y-1/2 cursor-pointer overflow-hidden rounded-full bg-gradient-to-tr from-black to-black/80 hover:w-72'
+      >
+        <span className='eq absolute left-1/2 top-1/2 z-[2] -translate-x-1/2 -translate-y-1/2 text-5xl text-white group-hover:left-10 group-hover:text-3xl'>
+          <FaMapLocationDot />
+        </span>
+        <span className='eq absolute left-full top-1/2 z-[2] -translate-y-1/2 whitespace-nowrap text-xl text-white group-hover:left-[4.5rem]'>
+          Explore Destinations
+        </span>
+      </Link>
     </section>
   );
 };

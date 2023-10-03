638c8716a6b24809539e3b8ea7d04b8dbe26c4c6
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-04 00:43:44 +0600

    register page added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -18,14 +18,22 @@ import NavbarMini from '@/components/shared/navbar/navbar-mini';
 import Button from '@/components/ui/button';
 
 interface FormData {
+  name: string;
+  photoUrl: string;
   email: string;
   password: string;
+  address?: string;
+  phoneNumber?: string;
 }
 
-const SignInPage = () => {
+const SignUpPage = () => {
   const [formData, setFormData] = useState<FormData>({
+    name: '',
+    photoUrl: '',
     email: '',
     password: '',
+    address: '',
+    phoneNumber: '',
   });
   const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
 
@@ -49,8 +57,12 @@ const SignInPage = () => {
       .subscribe(() => {
         console.log(formData);
         setFormData({
+          name: '',
+          photoUrl: '',
           email: '',
           password: '',
+          address: '',
+          phoneNumber: '',
         });
         setFormSubmitted(true);
       });
@@ -101,9 +113,45 @@ const SignInPage = () => {
 
           <div className='col-span-4 flex h-full w-full justify-center  px-20 pt-40'>
             <form id='form' className='h-full w-1/2 ' onSubmit={handleSubmit}>
-              <h4>Welcome back!</h4>
+              <h4>Create an account</h4>
               <p className='mb-5 text-black/30'>Please enter your deatils</p>
               <div className='mb-5 flex flex-col gap-2.5'>
+                <div className='flex flex-col gap-2'>
+                  <label
+                    htmlFor='name'
+                    className='eq cursor-pointer self-start hover:text-black/80'
+                  >
+                    Name
+                  </label>
+                  <input
+                    className='eq w-full appearance-none rounded-lg border border-black/30 bg-transparent px-3 py-2.5 outline-none hover:border-black/50 focus:border-black'
+                    type='name'
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    placeholder='Sarah Parker'
+                    id='name'
+                    name='name'
+                  />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                  <label
+                    htmlFor='photoUrl'
+                    className='eq cursor-pointer self-start hover:text-black/80'
+                  >
+                    Photo URL
+                  </label>
+                  <input
+                    className='eq w-full appearance-none rounded-lg border border-black/30 bg-transparent px-3 py-2.5 outline-none hover:border-black/50 focus:border-black'
+                    type='photoUrl'
+                    value={formData.photoUrl}
+                    onChange={handleInputChange}
+                    placeholder='Paste your photo url from pexels or cloudinary'
+                    id='photoUrl'
+                    name='photoUrl'
+                  />
+                </div>
+
                 <div className='flex flex-col gap-2'>
                   <label
                     htmlFor='email'
@@ -134,22 +182,58 @@ const SignInPage = () => {
                     type='password'
                     value={formData.password}
                     onChange={handleInputChange}
-                    name='password'
                     placeholder='enter your password'
                     id='password'
+                    name='password'
+                  />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                  <label
+                    htmlFor='address'
+                    className='eq cursor-pointer self-start hover:text-black/80'
+                  >
+                    Address (Optional)
+                  </label>
+                  <input
+                    className='eq w-full appearance-none rounded-lg border border-black/30 bg-transparent px-3 py-2.5 outline-none hover:border-black/50 focus:border-black'
+                    type='password'
+                    value={formData.address}
+                    onChange={handleInputChange}
+                    placeholder='123 Main Street, TA'
+                    id='address'
+                    name='address'
+                  />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                  <label
+                    htmlFor='phoneNumber'
+                    className='eq cursor-pointer self-start hover:text-black/80'
+                  >
+                    Phone number (Optional)
+                  </label>
+                  <input
+                    className='eq w-full appearance-none rounded-lg border border-black/30 bg-transparent px-3 py-2.5 outline-none hover:border-black/50 focus:border-black'
+                    type='phoneNumber'
+                    value={formData.phoneNumber}
+                    onChange={handleInputChange}
+                    placeholder='+123456789'
+                    id='phoneNumber'
+                    name='phoneNumber'
                   />
                 </div>
               </div>
               <Button type='submit' size={'full'}>
-                Login
+                Register
               </Button>
               <p className='mt-2.5'>
-                Don&apos;t have an account?{' '}
+                Already have an account?{' '}
                 <Link
                   className='eq font-semibold text-envy hover:text-envy/80'
-                  href={'/sign-up'}
+                  href={'/sign-in'}
                 >
-                  Register
+                  Login
                 </Link>
               </p>
             </form>
@@ -161,4 +245,4 @@ const SignInPage = () => {
   );
 };
 
-export default SignInPage;
+export default SignUpPage;
--- components/shared/navbar/navbar-mini.tsx
+++ components/shared/navbar/navbar-mini.tsx
@@ -1,5 +1,34 @@
+'use client';
+
+import CustomLink from '@/components/ui/custom-link';
+import { useScrollContext } from '@/contexts/scroll-context';
+import { cn } from '@/lib/utils';
+
 const NavbarMini = () => {
-  return <header className='h-20'>NavbarMini</header>;
+  const { isScrolled } = useScrollContext();
+
+  return (
+    <header
+      className={cn(
+        'eq flex h-20 items-center',
+        isScrolled && 'fixed left-0 right-0 top-0 z-[100] w-full bg-cream'
+      )}
+    >
+      <div className='container flex items-center justify-center'>
+        <nav>
+          <ul>
+            <li className='flex h-full items-center'>
+              <CustomLink
+                href='/'
+                title='Vista Safari'
+                className='text-xl font-semibold uppercase tracking-widest'
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default NavbarMini;
